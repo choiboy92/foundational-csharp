390b4f905c27c94e77fe9c00c082bf8ac2c178f8
Junho Choi <junhochoi@Junhos-Air.home>
2023-09-17 22:18:43 +0100

    object and methods lesson

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
