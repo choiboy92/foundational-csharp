8333c7a6bc425da80c2f699eb336de820dfa5256
Junho Choi <junhochoi@Junhos-Air.home>
2023-09-17 22:34:05 +0100

    if, if-else statement lesson

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,8 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-largerValue = Math.Max(firstValue, secondValue);
+// Your code goes here
 
-Console.WriteLine(largerValue);
+if (daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1) {
+    Console.WriteLine("Your subscription expires within a day.");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
