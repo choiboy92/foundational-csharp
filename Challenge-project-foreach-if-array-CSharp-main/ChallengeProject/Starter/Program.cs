d8d62460420777035af7a60d5a23003e9090dc4d
Junho Choi <junhochoi13@gmail.com>
2023-09-19 22:45:07 +0100

    complete challenge project

--- Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
+++ Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/Program.cs
@@ -33,7 +33,7 @@ string currentStudentLetterGrade = "";
 
 // display the header row for scores/grades
 Console.Clear();
-Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall\tGrade\tExtra Credit\n");
 
 /*
 The outer foreach loop is used to:
@@ -59,12 +59,16 @@ foreach (string name in studentNames)
     else if (currentStudent == "Logan")
         studentScores = loganScores;
 
-    int sumAssignmentScores = 0;
+    decimal sumAssignmentScores = 0;
 
     decimal currentStudentGrade = 0;
 
     int gradedAssignments = 0;
 
+    // counter for number of extra assignments completed
+    int extraAssignments = 0;
+    int extraAssignmentSum = 0;
+    int examSum = 0;
     /* 
     the inner foreach loop sums assignment scores
     extra credit assignments are worth 10% of an exam score
@@ -74,11 +78,22 @@ foreach (string name in studentNames)
         gradedAssignments += 1;
 
         if (gradedAssignments <= examAssignments)
-            sumAssignmentScores += score;
-
+        {
+            examSum += score;
+        }
         else
-            sumAssignmentScores += score / 10;
+        {
+            extraAssignments += 1;
+            extraAssignmentSum += score;
+        }
     }
+    decimal examScore = (decimal) examSum / examAssignments;
+    decimal extraCreditScore = (decimal) extraAssignmentSum / extraAssignments;
+
+    sumAssignmentScores = examSum + ((decimal)extraAssignmentSum / 10);
+    
+    decimal extraCreditPoints =  ( (decimal) extraAssignmentSum/10)/examAssignments;
+
 
     currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
 
@@ -124,7 +139,7 @@ foreach (string name in studentNames)
     // Student         Grade
     // Sophia:         92.2    A-
     
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+    Console.WriteLine($"{currentStudent}\t\t{examScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{extraCreditScore} ({extraCreditPoints}pts)");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
