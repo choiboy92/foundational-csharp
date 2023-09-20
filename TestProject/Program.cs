603560817bbfdbf8a36cf74ee1add6024340f01d
Junho Choi <junhochoi13@gmail.com>
2023-09-20 21:40:59 +0100

    code blocks

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,29 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*
-Reverse the chars
-convert the message into a char array
-count the o's
-convert it back to a string
-print it out
-*/
 
-
-string str = "The quick brown fox jumps over the lazy dog.";
-
-char[] charMessage = str.ToCharArray();
-Array.Reverse(charMessage);
-
-int x = 0;
-
-foreach (char i in charMessage)
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+foreach (int number in numbers)
 {
-    if (i == 'o')
-    {
-        x++;
-    }
+    total += number;
+    if (number == 42)
+       found = true;
 }
 
-string new_message = new String(charMessage);
+if (found) 
+    Console.WriteLine("Set contains 42");
 
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {x} times.");
+Console.WriteLine($"Total: {total}");
